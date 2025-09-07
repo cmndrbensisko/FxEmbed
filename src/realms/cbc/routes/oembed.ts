2c4fa921a9530dfb7a973e5c6cfd08d42d16703a
Adam Drackley <adam.drackley@gmail.com>
2025-09-07 01:02:39 -0400

    test

--- src/realms/cbc/routes/oembed.ts
+++ src/realms/cbc/routes/oembed.ts
@@ -27,7 +27,9 @@ const article = await fetchArticle()
     const detailHeadline = article.substring(article.indexOf('name="twitter:title" content="') + 30,article.indexOf('">',article.indexOf('name="twitter:title" content="') + 30))
     const detailSummary = article.substring(article.indexOf('name="twitter:description" content="') + 36,article.indexOf('">',article.indexOf('name="twitter:description" content="') + 36))
     const detailImage = article.substring(article.indexOf('name="twitter:image" content="') + 30,article.indexOf('">',article.indexOf('name="twitter:image" content="') + 30))
-  const data = '<title>Website Name</title><meta content="' + detailHeadline + '" property="og:title" /><meta content="' + detailSummary + '" property="og:description" /><meta content="' + c.req.url.replace("fxcbc","cbc") + '" property="og:url" /><meta content="' + detailImage + '" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
+  const imageHeight = article.substring(article.indexOf('name="twitter:player:width" content="') + 37,article.indexOf('">',article.indexOf('name="twitter:player:width" content="') + 37))
+  const imageWidth  = article.substring(article.indexOf('name="twitter:player:height" content="') + 38,article.indexOf('">',article.indexOf('name="twitter:player:height" content="') + 38))
+  const data = '<title>CBC News</title><meta content="' + detailHeadline + '" property="og:title" /><meta content="' + detailSummary + '" property="og:description" /><meta content="' + c.req.url.replace("fxcbc","cbc") + '" property="og:url" /><meta content="' + detailImage + '" property="og:image" /><meta property="og:image:width" content="'+ imageWidth + '/><meta property="og:image:height" content="'+ imageHeight + '"/><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
   /* Stringify and send it on its way! */
   return c.html(data, 200);
 };
