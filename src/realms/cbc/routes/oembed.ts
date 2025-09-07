c415cfd719091f55d71994ee2d862c4a0a6ae33c
Adam Drackley <adam.drackley@gmail.com>
2025-09-07 00:12:39 -0400

    test

--- src/realms/cbc/routes/oembed.ts
+++ src/realms/cbc/routes/oembed.ts
@@ -23,7 +23,7 @@ const fetchArticle = async (): Promise<null> => {
   return await res.text();
 };
 
-article = await fetchArticle()
+const article = await fetchArticle()
   const data = '<title>Website Name</title><meta content="' + document.querySelector("detailHeadline").innerHTML + '" property="og:title" /><meta content="Site Description" property="og:description" /><meta content="https://embed.com/this-is-the-site-url" property="og:url" /><meta content="https://embed.com/embedimage.png" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
   /* Stringify and send it on its way! */
   return c.html(data, 200);
