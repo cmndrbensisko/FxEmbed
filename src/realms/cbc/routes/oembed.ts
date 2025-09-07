0c0dc4b4d0be4670e24e0fde0b7947b4f66e3d0f
Adam Drackley <adam.drackley@gmail.com>
2025-09-07 01:08:12 -0400

    test

--- src/realms/cbc/routes/oembed.ts
+++ src/realms/cbc/routes/oembed.ts
@@ -29,6 +29,11 @@ const article = await fetchArticle()
     const detailImage = article.substring(article.indexOf('name="twitter:image" content="') + 30,article.indexOf('">',article.indexOf('name="twitter:image" content="') + 30))
   const imageHeight = article.substring(article.indexOf('name="twitter:player:width" content="') + 37,article.indexOf('">',article.indexOf('name="twitter:player:width" content="') + 37))
   const imageWidth  = article.substring(article.indexOf('name="twitter:player:height" content="') + 38,article.indexOf('">',article.indexOf('name="twitter:player:height" content="') + 38))
+  console.log(detailHeadline);
+  console.log(detailSummary);
+  console.log(detailImage);
+  console.log(imageHeight);
+  console.log(imageWidth);
   const data = '<title>CBC News</title><meta content="' + detailHeadline + '" property="og:title" /><meta content="' + detailSummary + '" property="og:description" /><meta content="' + detailImage + '" property="og:image" /><meta property="og:image:secure_url" content="' + detailImage + '" /><meta property="og:image:type" content="image/jpeg" /> <meta content="' + c.req.url.replace("fxcbc","cbc") + '" property="og:url" /><meta property="og:image:width" content="'+ imageWidth + '/><meta property="og:image:height" content="'+ imageHeight + '"/><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
   /* Stringify and send it on its way! */
   return c.html(data, 200);
