5deb4cd3638168a2e822e2d16e5773c2f48f60a0
Adam Drackley <adam.drackley@gmail.com>
2025-09-06 23:59:27 -0400

    test

--- src/realms/cbc/routes/oembed.ts
+++ src/realms/cbc/routes/oembed.ts
@@ -16,7 +16,7 @@ export const oembed = async (c: Context) => {
   const statusUrl = `${Constants.BSKY_ROOT}/profile/${encodeURIComponent(author)}/post/${status}`;
   const branding = getBranding(c);
 
-  const data = '<title>Website Name</title><meta content="Embed Title" property="og:title" /><meta content="Site Description" property="og:description" /><meta content="https://embed.com/this-is-the-site-url" property="og:url" /><meta content="https://embed.com/embedimage.png" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><div onload="window.location.replace(\'' + c.req.url + '\')"></div>';
+  const data = '<title>Website Name</title><meta content="Embed Title" property="og:title" /><meta content="Site Description" property="og:description" /><meta content="https://embed.com/this-is-the-site-url" property="og:url" /><meta content="https://embed.com/embedimage.png" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><div onload="window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')"></div>';
   /* Stringify and send it on its way! */
   return c.html(data, 200);
 };
