2627dcdda8fd5ecb21e232713df7fb7e222506ca
Adam Drackley <adam.drackley@gmail.com>
2025-09-07 00:37:35 -0400

    test

--- src/realms/cbc/routes/oembed.ts
+++ src/realms/cbc/routes/oembed.ts
@@ -24,12 +24,12 @@ const fetchArticle = async (): Promise<null> => {
 };
 
 const article = await fetchArticle()
-    const parser = new DOMParser()
-
+    const detailHeadline = article.substring(article.indexOf('data-cy="detailHeadline">') + 25,article.indexOf('</h1>',article.indexOf('data-cy="detailHeadline">') + 25))
+    const detailSummary = article.substring(article.indexOf('class="detailSummary">') + 22,article.indexOf('</div>',article.indexOf('class="detailSummary">') + 22))
     // Parse the text
     const doc = parser.parseFromString(article, "text/html")
 
-  const data = '<title>Website Name</title><meta content="' + doc.querySelector("detailHeadline").innerHTML + '" property="og:title" /><meta content="' + doc.querySelector("detailSummary").innerHTML + '" property="og:description" /><meta content="https://embed.com/this-is-the-site-url" property="og:url" /><meta content="https://embed.com/embedimage.png" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
+  const data = '<title>Website Name</title><meta content="' + detailHeadline + '" property="og:title" /><meta content="' + detailSummary + '" property="og:description" /><meta content="https://embed.com/this-is-the-site-url" property="og:url" /><meta content="https://embed.com/embedimage.png" property="og:image" /><meta content="#43B581" data-react-helmet="true" name="theme-color" /><script>window.onload = window.location.replace(\'' + c.req.url.replace("fxcbc","cbc") + '\')</script>';
   /* Stringify and send it on its way! */
   return c.html(data, 200);
 };
